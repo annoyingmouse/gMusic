8040497fa900b57d6e25e9e5d5a54f9dd9fb9b12
Dominic Myers <annoyingmouse@gmail.com>
2013-12-22 13:12:26 +0000

    Removed PNGs and added Canvas
    All thanks to:
    
    http://www.storminthecastle.com/2013/07/24/how-you-can-draw-regular-polygons-with-the-html5-canvas-api/
    and:
    http://smus.com/dynamic-icons-chrome-extensions/

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,62 @@
 var setPlayerIcon = function(playerIcon){
-    var iconPath = [
-        "disabled.png", 
-        "pause.png", 
-        "play.png"];
-    chrome.browserAction.setIcon({
-        path: "icons/" + iconPath[playerIcon]
-    });
-    if(playerIcon){
-        chrome.browserAction.enable();
-    }else{
-        chrome.browserAction.disable();
+    // http://www.storminthecastle.com/2013/07/24/how-you-can-draw-regular-polygons-with-the-html5-canvas-api/
+    // http://smus.com/dynamic-icons-chrome-extensions/
+    function polygon(ctx, x, y, radius, sides, startAngle, anticlockwise) {
+        if (sides < 3) return;
+        var a = (Math.PI * 2)/sides;
+        a = anticlockwise?-a:a;
+        ctx.save();
+        ctx.translate(x,y);
+        ctx.rotate(startAngle);
+        ctx.moveTo(radius,0);
+        for (var i = 1; i < sides; i++) {
+            ctx.lineTo(radius*Math.cos(a*i),radius*Math.sin(a*i));
+        }
+        ctx.closePath();
+        ctx.restore();
     }
+    var cv = document.getElementById('canvas');
+    if (cv != null){
+        document.body.removeChild(cv);
+    }
+    var cv = document.createElement('canvas');
+        cv.setAttribute('id', 'canvas');
+        cv.setAttribute('height', '19');
+        cv.setAttribute('width', '19');
+    document.body.appendChild(cv)
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+        context.beginPath();
+        context.arc(9.5, 9.5, 9.5, 0, 2 * Math.PI, false);
+        context.fillStyle = '#f6f6f6';
+        context.fill();
+    switch (playerIcon){
+        case 0: 
+                context.beginPath();
+            polygon(context, 9.5, 9.5, 7, 3, 180*Math.PI/2);
+                context.fillStyle = '#aaaaaa';
+                context.fill();
+            break;
+
+        case 1:
+                context.beginPath();
+                context.rect(5, 4, 3, 11);
+                context.rect(11, 4, 3, 11);
+                context.fillStyle = '#333333';
+                context.fill();
+            break;
+        case 2:
+                context.beginPath();
+                polygon(context, 9.5, 9.5, 7, 3, 180*Math.PI/2);
+                context.fillStyle = '#333333';
+                context.fill();
+            break;
+    }
+    var imageData = context.getImageData(0, 0, 19, 19);
+    chrome.browserAction.setIcon({
+        imageData: imageData
+    });
+    (playerIcon) ? chrome.browserAction.enable() : chrome.browserAction.disable();
 }
 var playerState = 0;
 setPlayerIcon(0);
