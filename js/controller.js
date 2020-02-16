31eb45c37d9324c1052f58b79edbc31159c85582
Dominic Myers <annoyingmouse@gmail.com>
2020-02-16 11:41:55 +0000

    Now working for 9128

--- js/controller.js
+++ js/controller.js
@@ -1,79 +1,82 @@
-var setPlayerIcon = function (playerIcon) {
-    function polygon (ctx, x, y, radius, sides, startAngle, anticlockwise) {
-        if (sides < 3) return;
-        var a = (Math.PI * 2) / sides;
-        a = anticlockwise ? -a : a;
-        ctx.save();
-        ctx.translate(x, y);
-        ctx.rotate(startAngle);
-        ctx.moveTo(radius, 0);
-        for (var i = 1; i < sides; i++) {
-            ctx.lineTo(radius * Math.cos(a * i), radius * Math.sin(a * i));
-        }
-        ctx.closePath();
-        ctx.restore();
-    }
 
-    var cv = document.getElementById('canvas');
-    if (cv) {
-        document.body.removeChild(cv);
+const setPlayerIcon = (playerIcon) => {
+  const polygon = (ctx, x, y, radius, sides, startAngle, anticlockwise) => {
+    if (sides < 3) return
+    let a = (Math.PI * 2) / sides
+    a = anticlockwise ? -a : a
+    ctx.save()
+    ctx.translate(x, y)
+    ctx.rotate(startAngle)
+    ctx.moveTo(radius, 0)
+    for (var i = 1; i < sides; i++) {
+      ctx.lineTo(radius * Math.cos(a * i), radius * Math.sin(a * i))
     }
-    cv = document.createElement('canvas');
-    cv.setAttribute('id', 'canvas');
-    cv.setAttribute('height', '19');
-    cv.setAttribute('width', '19');
-    document.body.appendChild(cv);
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    context.beginPath();
-    context.arc(9.5, 9.5, 9.5, 0, 2 * Math.PI, false);
-    context.fillStyle = '#fff8e6';
-    context.fill();
-    switch (playerIcon) {
-        case 0:
-            context.beginPath();
-            polygon(context, 9.5, 9.5, 7, 3, 180 * Math.PI / 2);
-            context.fillStyle = '#fff8e6';
-            context.fill();
-            break;
-        case 1:
-            context.beginPath();
-            context.rect(5, 4, 3, 11);
-            context.rect(11, 4, 3, 11);
-            context.fillStyle = '#00d4c3';
-            context.fill();
-            break;
-        case 2:
-            context.beginPath();
-            polygon(context, 9.5, 9.5, 7, 3, 180 * Math.PI / 2);
-            context.fillStyle = '#00d4c3';
-            context.fill();
-            break;
+    ctx.closePath()
+    ctx.restore()
+  }
+  let cv = document.getElementById('canvas')
+  if (cv !== null) {
+    document.body.removeChild(cv)
+  }
+  cv = document.createElement('canvas')
+  cv.setAttribute('id', 'canvas')
+  cv.setAttribute('height', '19')
+  cv.setAttribute('width', '19')
+  document.body.appendChild(cv)
+  var canvas = document.getElementById('canvas')
+  var context = canvas.getContext('2d')
+  context.beginPath()
+  context.arc(9.5, 9.5, 9.5, 0, 2 * Math.PI, false)
+  context.fillStyle = '#fff8e6'
+  context.fill()
+  const icons = [
+    () => {
+      context.beginPath()
+      polygon(context, 9.5, 9.5, 7, 3, 180 * Math.PI / 2)
+      context.fillStyle = '#5fe1d0'
+      context.fill()
+    },
+    () => {
+      context.beginPath()
+      context.rect(5, 4, 3, 11)
+      context.rect(11, 4, 3, 11)
+      context.fillStyle = '#00d4c3'
+      context.fill()
+    },
+    () => {
+      context.beginPath()
+      polygon(context, 9.5, 9.5, 7, 3, 180 * Math.PI / 2)
+      context.fillStyle = '#00d4c3'
+      context.fill()
     }
-    var imageData = context.getImageData(0, 0, 19, 19);
-    chrome.browserAction.setIcon({
-        imageData: imageData
-    });
-};
-var playerState = 0;
-setPlayerIcon(0);
-var port = chrome.runtime.connect({
-    name: "9128Controller"
-});
-var handleConnect = function (port) {
-    function listener () {
-        port.postMessage({ command: playerState });
-    }
+  ]
+  icons[playerIcon]()
+  const imageData = context.getImageData(0, 0, 19, 19)
+  chrome.browserAction.setIcon({
+    imageData: imageData
+  })
+}
+let playerState = 0
+setPlayerIcon(0)
 
-    port.onMessage.addListener(function (msg) {
-        playerState = msg.player;
-        setPlayerIcon(playerState);
-    });
-    port.onDisconnect.addListener(function (port) {
-        port = null;
-        setPlayerIcon(0);
-        chrome.browserAction.onClicked.removeListener(listener)
-    });
-    chrome.browserAction.onClicked.addListener(listener)
-};
-chrome.runtime.onConnect.addListener(handleConnect);
+const handleConnect = port => {
+  if (!port) {
+    port = chrome.runtime.connect({
+      name: '9128Control'
+    })
+  }
+  const listener = () => {
+    port.postMessage({ command: playerState })
+  }
+  port.onMessage.addListener(function (msg) {
+    playerState = msg.player
+    setPlayerIcon(playerState)
+  })
+  port.onDisconnect.addListener(function (port) {
+    port = null
+    setPlayerIcon(0)
+    chrome.browserAction.onClicked.removeListener(listener)
+  })
+  chrome.browserAction.onClicked.addListener(listener)
+}
+chrome.runtime.onConnect.addListener(handleConnect)
--- js/injected.js
+++ js/injected.js
@@ -1,28 +1,24 @@
-var port = chrome.runtime.connect({
-    name: "9128Controller"
-});
-var checkExist = setInterval(function () {
-    if (document.getElementById('playButton')) {
-        var playerButton = document.getElementById('playButton')
-        var observer = new WebKitMutationObserver(function (mutations) {
-            getStatus()
-        });
-
-        function getStatus () {
-            port.postMessage({
-                player: playerButton.classList.contains('icon-playerstop') ? 1 : 2
-            });
-        }
-
-        observer.observe(playerButton, {
-            attributes: true,
-            subtree: false
-        });
-        port.onMessage.addListener(function (msg) {
-            if (msg.command) {
-                playerButton.click();
-            }
-        });
-        clearInterval(checkExist);
+const port = chrome.runtime.connect({
+  name: '9128Control'
+})
+const checkExist = setInterval(() => {
+  if (document.getElementById('playButton')) {
+    const playerButton = document.getElementById('playButton')
+    const observer = new WebKitMutationObserver(function (mutations) {
+      getStatus()
+    })
+    const getStatus = () => {
+      port.postMessage({
+        player: playerButton.classList.contains('icon-playerstop') ? 1 : 2
+      })
     }
-}, 1000);
+    observer.observe(playerButton, {
+      attributes: true,
+      subtree: false
+    })
+    port.onMessage.addListener(function (msg) {
+      msg.command && playerButton.click()
+    })
+    clearInterval(checkExist)
+  }
+}, 1000)
