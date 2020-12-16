720b24e3403cc244af8005bb9785244b68c880e3
Dominic Myers <annoyingmouse@gmail.com>
2020-12-16 18:54:26 +0000

    pre pull

--- js/injected.js
+++ js/injected.js
@@ -1,31 +1,54 @@
 const port = chrome.runtime.connect({
-    name: "aMusic"
-});
-const checkExist = setInterval(function () {
+    name: 'a_Music'
+})
+const checkExist = setInterval(() => {
     if (document.getElementById("transport")){
         const transport = document.getElementById("transport")
         if (transport.querySelectorAll('music-button[size=medium]').length) {
-            const playerButton = transport.querySelector('music-button[size=medium]');
-            const observer = new WebKitMutationObserver(function (mutations) {
-                getStatus();
-            });
-
-            function getStatus() {
-                port.postMessage({
-                    player: playerButton.getAttribute("aria-label") === "Pause" ? 1 : 2
-                });
-            }
-
+            const playerButton = transport.querySelector('music-button[size=medium]')
+            const observer = new MutationObserver(() => port.postMessage({
+                player: playerButton.getAttribute("aria-label") === "Pause" ? 1 : 2
+            }))
             observer.observe(playerButton, {
                 attributes: true,
                 subtree: false
-            });
+            })
             port.onMessage.addListener(function (msg) {
-                if (msg.command) {
-                    playerButton.click();
-                }
-            });
-            clearInterval(checkExist);
+                msg.command && playerButton.click()
+            })
+            clearInterval(checkExist)
         }
     }
-}, 100);
+}, 100)
+
+// const port = chrome.runtime.connect({
+//     name: "aMusic"
+// });
+// const checkExist = setInterval(function () {
+//     if (document.getElementById("transport")){
+//         const transport = document.getElementById("transport")
+//         if (transport.querySelectorAll('music-button[size=medium]').length) {
+//             const playerButton = transport.querySelector('music-button[size=medium]');
+//             const observer = new WebKitMutationObserver(function (mutations) {
+//                 getStatus();
+//             });
+//
+//             function getStatus() {
+//                 port.postMessage({
+//                     player: playerButton.getAttribute("aria-label") === "Pause" ? 1 : 2
+//                 });
+//             }
+//
+//             observer.observe(playerButton, {
+//                 attributes: true,
+//                 subtree: false
+//             });
+//             port.onMessage.addListener(function (msg) {
+//                 if (msg.command) {
+//                     playerButton.click();
+//                 }
+//             });
+//             clearInterval(checkExist);
+//         }
+//     }
+// }, 100);
