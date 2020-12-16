aaa08145564b04568f60c667a9739f16f050673b
Dominic Myers <annoyingmouse@gmail.com>
2020-12-16 18:44:41 +0000

    Much better

--- js/injected.js
+++ js/injected.js
@@ -2,30 +2,27 @@ const port = chrome.runtime.connect({
     name: "aMusic"
 });
 const checkExist = setInterval(function () {
-    if (document.getElementById("transport")){
-        const transport = document.getElementById("transport")
-        if (transport.querySelectorAll('music-button[size=medium]').length) {
-            const playerButton = transport.querySelector('music-button[size=medium]');
-            const observer = new WebKitMutationObserver(function (mutations) {
-                getStatus();
-            });
+    if (document.querySelectorAll('#transport music-button[size=medium]').length) {
+        const playerButton = document.querySelector('#transport music-button[size=medium]');
+        const observer = new WebKitMutationObserver(function (mutations) {
+            getStatus();
+        });
 
-            function getStatus() {
-                port.postMessage({
-                    player: playerButton.getAttribute("aria-label") === "Pause" ? 1 : 2
-                });
-            }
-
-            observer.observe(playerButton, {
-                attributes: true,
-                subtree: false
+        function getStatus() {
+            port.postMessage({
+                player: playerButton.getAttribute("aria-label") === "Pause" ? 1 : 2
             });
-            port.onMessage.addListener(function (msg) {
-                if (msg.command) {
-                    playerButton.click();
-                }
-            });
-            clearInterval(checkExist);
         }
+
+        observer.observe(playerButton, {
+            attributes: true,
+            subtree: false
+        });
+        port.onMessage.addListener(function (msg) {
+            if (msg.command) {
+                playerButton.click();
+            }
+        });
+        clearInterval(checkExist);
     }
 }, 100);
